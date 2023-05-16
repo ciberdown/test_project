50c835384c09cf342b603770b8b3bc0d9cb50294
ciberdown <teymuri.amin@gmail.com>
2023-05-16 21:38:45 +0330

    nonet icon added

--- src/components/UI/loadingComponent.jsx
+++ src/components/UI/loadingComponent.jsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
+import NoNetworkIcon from "./noNetwork";
 
 export default function Loading(props) {
   return (
     <div style={{ display: "flex", justifyContent: "center" }}>
       {props.loading}
       {props.loading === "Loading..." && <LoadingIcon />}
+      {props.loading === "network error!" && <NoNetworkIcon />}
     </div>
   );
 }
---
+++ src/components/UI/noNetwork.jsx
@@ -0,0 +1,7 @@
+import PermScanWifiIcon from "@mui/icons-material/PermScanWifi";
+import { Component } from "react";
+export default class NoNetworkIcon extends Component {
+  render() {
+    return <PermScanWifiIcon sx={{ color: "red" }} />;
+  }
+}
